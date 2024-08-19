55dc7f78d13d2424c00ebc67a23f7b0012df0642
atstahir <atstahir@outlook.com>
2024-08-20 03:52:38 +0500

    added SendMessageAsync method in ChatHub class

--- SignalRDemo/Hubs/ChatHub.cs
+++ SignalRDemo/Hubs/ChatHub.cs
@@ -1,7 +1,23 @@
 ﻿using Microsoft.AspNet.SignalR;
+using SignalRDemo.Models;
 
 namespace SignalRDemo.Hubs;
 
 public class ChatHub : Hub
 {
+    public async Task SendMessageAsync(string name, string text) 
+    {
+        var message = new ChatMessage 
+        {
+            SenderName = name,
+            Text = text,
+            SentAt = DateTimeOffset.UtcNow,            
+        };
+
+        await Clients.All.SendAsync(
+            "RecieveMessage",
+            message.SenderName,
+            message.Text,
+            message.SentAt);
+    }
 }
