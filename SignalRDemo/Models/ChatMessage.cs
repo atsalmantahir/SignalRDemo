a7d287f717ba9d20a37570b661dfe367ed98e70a
atstahir <atstahir@outlook.com>
2024-08-20 03:49:12 +0500

    added chatMessage model

---
+++ SignalRDemo/Models/ChatMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalRDemo.Models;
+
+public class ChatMessage
+{
+    public string? SenderName { get; set; }
+    public string? Text { get; set; }
+    public DateTimeOffset SentAt { get; set; }
+}
